60bfbe32ccc0d74677053f26c4dacb1ae5486b03
nakajima <nakajima@nakajimanoMacBook-ea.local>
2020-05-13 17:03:14 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -11,6 +11,19 @@ class Vec3{
         this.y += v.y;
         this.z += v.z;
     }
+
+    AreaOfTriangle(v1,v2){
+        v1.x -= this.x;
+        v1.y -= this.y;
+        v1.z -= this.z;
+        v2.x -= this.x;
+        v2.y -= this.y;
+        v2.z -= this.z;
+        var area;
+        area=0.5*Math.sqrt((v1.x*v1.x+v1.y*v1.y+v1.z*v1.z)*(v2.x*v2.x+v2.y*v2.y+v2.z*v2.z));
+        return area;
+    }
+
     sum(){
         sum=this.x+this.y+this.z;
         return sum;
@@ -39,17 +52,17 @@ class Vec3{
         mid=this.x;
         if((mid>this.y && mid>this.z)){
             if(this.y>this.z){
-                mid=this.z;
-            }else{
                 mid=this.y;
+            }else{
+                mid=this.z;
             }
             return mid;
         }
         if((mid<this.y && mid<this.z)){
             if(this.y>this.z){
-                mid=this.y;
-            }else{
                 mid=this.z;
+            }else{
+                mid=this.y;
             }
             return mid;
         }
